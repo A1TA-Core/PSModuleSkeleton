b65ef787f21f5b4685ea45d408717250baf7e1fe
Markus Feiler <chaozmc@is-jo.org>
2020-03-01 16:16:43 +0100

    updated

--- Publish-MyModule.ps1
+++ Publish-MyModule.ps1
@@ -1,8 +1,8 @@
 $PublishParams = @{
     NuGetApiKey = 'MyAPIKeyFromPowershellgallery.com' #Obtainable from powershellgallery.com
-    Path = '/Users/max/Documents/GitHub/PSModuleSkeleton'
-    ProjectUri = 'https://github.com/chaozmc/PSModuleSkeleton'
-    Tags = @('Some', 'really', 'cool', 'buzzwords' )
+    Path = '/Users/max/Documents/GitHub/PSModuleSkeleton' #Change this to fit your local path... C:\Users\max\source\Repos\PSModuleSkeleton for example on windows
+    ProjectUri = 'https://github.com/chaozmc/PSModuleSkeleton' #Change this to your website of the project, github for example
+    Tags = @('Some', 'really', 'cool', 'buzzwords' ) #Find some words to describe your module for easier finding in the PSGallery
 }
 
 Install-Module -Name PSScriptAnalyzer -force
